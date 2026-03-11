432190201f853a18226a6b8cd01ca84be2654f79
mapclean9r <tobias.the@hotmail.no>
2026-03-11 07:26:32 +0100

    w

--- src/AddTwoNumbers/Solution.java
+++ src/AddTwoNumbers/Solution.java
@@ -2,10 +2,31 @@ package AddTwoNumbers;
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode();
+        ListNode tmp1 = l1;
+        ListNode tmp2 = l2;
+        int numToMoveLeft = 0;
+        // tbc c:
 
 
+        do {
+            int addedNum = tmp1.val + tmp2.val + numToMoveLeft;
 
-        return l1;
+            if (addedNum > 9) {
+                int rightDigit = addedNum % 10;
+                numToMoveLeft = addedNum / 10;
+                listNode.next = new ListNode(rightDigit);
+            }
+            if (addedNum < 9) {
+                listNode.next = new ListNode(tmp1.val + tmp2.val + numToMoveLeft);
+                numToMoveLeft = 0;
+            }
+            tmp1 = new ListNode(tmp1.val);
+            tmp2 = new ListNode(tmp2.val);
+
+        } while (tmp1.next != null);
+
+        return listNode;
     }
 
     public static void main(String[] args) {
