5d5b9ea2f1a7f06cbe4fb3a247eb912e66dd729b
mapclean9r <tobias.the@hotmail.no>
2026-03-11 05:48:57 +0100

    Update Solution.java

--- src/AddTwoNumbers/Solution.java
+++ src/AddTwoNumbers/Solution.java
@@ -1,8 +1,24 @@
 package AddTwoNumbers;
 
 
+import java.util.LinkedList;
+
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+
+        return new ListNode();
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 =
+                new ListNode(2,
+                        new ListNode(4,
+                                new ListNode(3)));
+
+        ListNode listNode2 =
+                new ListNode(5,
+                        new ListNode(6,
+                                new ListNode(4)));
+
     }
 }
